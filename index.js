9f101b4c0a9b60571598b42eb2d76b3621c6f061
Tim Daubenschütz <tim.daubenschuetz@gmail.com>
2014-11-02 13:15:56 +0100

    init commit for resume

--- index.js
+++ index.js
@@ -2,10 +2,30 @@ var fs = require("fs");
 var Handlebars = require("handlebars");
 
 function render(resume) {
-	var css = fs.readFileSync(__dirname + "/style.css", "utf-8");
+
+	var css = fs.readFileSync(__dirname + "/css/style.css", "utf-8");
+  var opensans = fs.readFileSync(__dirname + "/bower_components/open-sans-fontface/open-sans.css", "utf-8");
+  var merriweather = fs.readFileSync(__dirname + "/bower_components/merriweather-fontface/merriweather.css", "utf-8");
 	var template = fs.readFileSync(__dirname + "/resume.template", "utf-8");
+
+  // http://stackoverflow.com/a/12002281/1263876
+  Handlebars.registerHelper("foreach",function(arr,options) {
+      if(options.inverse && !arr.length)
+          return options.inverse(this);
+
+      return arr.map(function(item,index) {
+          item.$index = index;
+          item.$first = index === 0;
+          item.$last  = index === arr.length-1;
+          return options.fn(item);
+      }).join('');
+  });
+
+  
 	return Handlebars.compile(template)({
 		css: css,
+    opensans: opensans,
+    merriweather: merriweather,
 		resume: resume
 	});
 }
